5b88c68e2cded77ad4e4e7802f96ad66161ac86e
eerotal <eerotal@users.noreply.github.com>
2018-01-18 07:41:00 +0200

    Modify the slide_names API endpoint for requesting arbitary data
    - Modify the slide_names API endpoint so that it can be used
      to request arbitary keys (that are still valid keys) from the
      server. This API endpoint returns the requested data from every
      existing slide. The endpoint was also renamed to
      slides_data_by_keys.php

--- src/api/endpoint/slides_data_by_keys.php
+++ src/api/endpoint/slides_data_by_keys.php
@@ -1,25 +1,37 @@
 <?php
 	/*
 	*
-	*  API handle to get a dictionary of all existing slide names.
+	*  An API handle to get specific data keys from all of the
+	*  currently existing slides.
+	*
+	*  GET parameters:
+	*    Data can be requested by just assigning the key name
+	*    a value such as 1 in the GET parameters.
 	*
 	*  Return value:
 	*    A JSON encoded dictionary with the following keys:
-	*      * names = The names dictionary. Contains id-name pairs. **
+	*      * data  = The requested data as nested dictionaries. **
 	*      * error = An error code or API_E_OK on success. ***
 	*
 	*    **  (Only exists if the call was successful.)
 	*    *** (The error codes are listed in api_errors.php.)
 	*/
 
+	require_once($_SERVER['DOCUMENT_ROOT'].'/global_php/util.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/api/api_util.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/api/api_errors.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/api/slide.php');
 
 	header_plaintext();
 
+	// Check that the requested keys are in SLIDE_REQ_KEYS.
+	if (!count($_GET) || !array_is_subset(
+		array_keys($_GET), SLIDE_REQ_KEYS)) {
+		error_and_exit(API_E_INVALID_REQUEST);
+	}
+
 	$slides = get_slides_id_list();
-	$ret = array('names' => array());
+	$ret = array('data' => array());
 	$tmp_slide = new Slide();
 
 	foreach($slides as $s) {
@@ -29,7 +41,11 @@
 		} catch (Exception $e) {
 			error_and_exit(API_E_INTERNAL);
 		}
-		$ret['names'][$s] = $tmp_slide->get('name');
+
+		$ret['data'][$s] = array();
+		foreach(array_keys($_GET) as $k) {
+			$ret['data'][$s][$k] = $tmp_slide->get($k);
+		}
 	}
 	$ret['error'] = API_E_OK;
 
