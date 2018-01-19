785de875cf1b9693b67dae5ab9a98669e7fc48aa
eerotal <eerotal@users.noreply.github.com>
2018-01-19 17:39:46 +0200

    Make the display show the slides in the correct order
    - Make the slide display show the slides in the correct order
      based on the slide indices.
    - Implement the rather convoluted but powerful utility function
      array_next(). The explanation of what the function does and how
      is long so it isn't included here, but a thorough writeup can
      be found in 'src/common/js/util.js' in the function definition.

--- src/app/js/renderer.js
+++ src/app/js/renderer.js
@@ -1,7 +1,42 @@
+var DEFAULT_RENDERER_UPDATE_INTERVAL = 5000;
 var SLIDES_RETRIEVE_INTERVAL = 30000;
 
 var display = $('#display');
-var c_slide = 0;
+var c_slide = null;
+
+function _renderer_next_slide(c_slide, wrap) {
+	var slides = slides_get();
+	if (slides.length) {
+		return array_next(
+			slides,
+			c_slide,
+			function(array, key) {
+				return array[key]['index'];
+			},
+			-1, // Start index when !c_slide.
+			function() {
+				/*
+				*  If no next exists, attempt to wrap
+				*  the 'search'. Note the wrap=false
+				*  argument on the _renderer_next_slide()
+				*  call below. This makes sure the
+				*  array_next calls don't create infinite
+				*  recursion if the slides array is empty,
+				*  since the next _renderer_next_slide()
+				*  call won't attempt to wrap and will
+				*  just return null instead.
+				*/
+				if (!wrap) {
+					return null;
+				}
+				return _renderer_next_slide(null,
+							false);
+			}
+		);
+	} else {
+		return null;
+	}
+}
 
 function renderer_animate(elem, animation, end_callback) {
 	/*
@@ -27,18 +62,25 @@ function renderer_update() {
 	if (!slides.length) { return; }
 
 	renderer_animate(display, 'swipe-left', function() {
-		display.html(slides[c_slide].markup);
+		c_slide = _renderer_next_slide(c_slide, true);
+		if (!c_slide) {
+			/*
+			*  Make sure the renderer continues to
+			*  update even if no slides currently exist.
+			*/
+			setTimeout(renderer_update,
+				DEFAULT_RENDERER_UPDATE_INTERVAL);
+			return;
+		}
+		display.html(c_slide.markup);
+
 		renderer_animate(display, 'swipe-from-right', null);
 
 		console.log("LibreImage: Changing slide in " +
-				slides[c_slide].time + "ms.");
+				c_slide.time + "ms.");
 
-		setTimeout(renderer_update, slides[c_slide].time);
+		setTimeout(renderer_update, c_slide.time);
 	});
-
-	if ((c_slide++) == slides.length - 1) {
-		c_slide = 0;
-	}
 }
 
 setInterval(function() {
@@ -51,3 +93,4 @@ list_retrieve(function() {
 		renderer_update();
 	});
 });
+
