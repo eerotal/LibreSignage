662c748c3557dd0c3c37e68def37f48a6fe222e6
eerotal <eerotal@users.noreply.github.com>
2018-04-08 14:48:56 +0300

    Implement convenience PHP functions for including JS libs

--- src/login/index.php
+++ src/login/index.php
@@ -1,6 +1,7 @@
 <?php
 	require_once($_SERVER['DOCUMENT_ROOT'].'/common/php/config.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/common/php/auth/auth.php');
+	require_once($_SERVER['DOCUMENT_ROOT'].'/common/php/js_include.php');
 
 	if (auth_is_authorized()) {
 		header('Location: '.LOGIN_LANDING);
@@ -69,18 +70,11 @@
 		</main>
 		<?php
 			require_once($_SERVER['DOCUMENT_ROOT'].FOOTER_PATH);
+
+			js_include_jquery();
+			js_include_popper();
+			js_include_bootstrap();
 		?>
-
-		<script src="https://code.jquery.com/jquery-3.3.1.min.js"
-			integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-			crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-			integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-			crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"
-			integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4"
-			crossorigin="anonymous"></script>
-
 		<script src="/common/js/util.js"></script>
 		<script src="/common/js/dialog.js"></script>
 		<script src="/common/js/cookie.js"></script>
