e099a5e115ebddb07b1e79c3869f8039b04639ea
eerotal <eerotal@users.noreply.github.com>
2018-07-28 13:15:52 +0300

    build: Improve log output from install.sh

--- build/scripts/install.sh
+++ build/scripts/install.sh
@@ -16,11 +16,11 @@ if [ ! -d "$APACHE_SITES" ]; then
 fi
 
 VHOST_DIR=`echo "$ICONF_DOCROOT/$ICONF_NAME" | sed "s/\/\+/\//g"`;
-echo "Virtual host dir: $VHOST_DIR";
+echo "[INFO] Virtual host dir: '$VHOST_DIR'";
 
 # Check whether VHOST_DIR already has files.
 if [ -n "`ls -A $VHOST_DIR`" ]; then
-	echo 'Virtual host directory is not empty.'
+	echo '[INFO] Virtual host directory is not empty.'
 	read -p 'Remove existing files and continue? (Y\N): ' read_val
 	case "$read_val" in
 		[Yy]* )
@@ -40,10 +40,10 @@ fi
 
 mkdir -p $VHOST_DIR;
 
-echo "Install LibreSignage to $VHOST_DIR";
-echo 'Copy files.';
+echo "[INFO] Install LibreSignage to '$VHOST_DIR'";
+echo '[INFO] Copy files.';
 cp -Rp $DIST_DIR/* $VHOST_DIR/.;
-echo 'Done!';
+echo '[INFO] Done!';
 
 ##
 ## Set the correct file permissions.
@@ -65,7 +65,7 @@ find "$VHOST_DIR/data" -type f -exec chmod 664 "{}" ";";
 ##  Create config and setup Apache.
 ##
 
-echo "Create VHost config. ($APACHE_SITES/$ICONF_NAME.conf)";
+echo "[INFO] Create VHost config in '$APACHE_SITES/$ICONF_NAME.conf'";
 if [ -f "$APACHE_SITES/$ICONF_NAME.conf" ]; then
 	read -p 'Replace existing VHost config? (Y\N): ' repl_vhost_conf
 	case "$repl_vhost_conf" in
@@ -78,20 +78,20 @@ if [ -f "$APACHE_SITES/$ICONF_NAME.conf" ]; then
 fi
 
 . 'build/scripts/vhost_template.sh' > $APACHE_SITES/$ICONF_NAME.conf;
-echo 'LibreSignage installed!';
+echo '[INFO] LibreSignage installed!';
 
-echo 'Enable apache2 mod_rewrite...';
+echo '[INFO] Enable apache2 mod_rewrite...';
 a2enmod rewrite;
 
 read -p 'Enable the created VHost and restart apache2? (Y\N): ' EN_VHOST;
 case "$EN_VHOST" in
 	[Yy]* )
-		echo "Enabling site '$ICONF_NAME.conf'...";
+		echo "[INFO] Enabling site '$ICONF_NAME.conf'...";
 		a2ensite "$ICONF_NAME.conf";
-		echo 'Restarting apache2...';
+		echo '[INFO] Restarting apache2...';
 		systemctl stop apache2
 		systemctl start apache2
-		echo 'Done!';
+		echo '[INFO] Done!';
 		;;
 	*)
 		;;
