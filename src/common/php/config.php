b586daf973574b397be0c1876223b4115d0b03d5
eerotal <eerotal@users.noreply.github.com>
2018-12-04 15:54:29 +0200

    config: Disable video thumbnail generation by default

--- src/common/php/config.php
+++ src/common/php/config.php
@@ -55,7 +55,7 @@
 	const AUTH_TOKEN_LEN			= 15;
 	const SLIDE_LOCK_MAX_AGE		= 600;
 
-	const ENABLE_FFMPEG_THUMBS      = TRUE;
+	const ENABLE_FFMPEG_THUMBS      = FALSE;
 	const ENABLE_GD_THUMBS          = TRUE;
 
 	const FFMPEG_PATH               = '/usr/bin/ffmpeg';
