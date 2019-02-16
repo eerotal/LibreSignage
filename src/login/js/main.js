7551fbb9ec9f3039ebf1b683c8e9f955d231a87d
eerotal <eerotal@users.noreply.github.com>
2019-02-16 13:51:43 +0200

    login: Rewrite the login page JS to use the MVC model

--- src/login/js/main.js
+++ src/login/js/main.js
@@ -1,140 +1,20 @@
+/*
+*  JavaScript entry point for the Login page.
+*/
 var $ = require('jquery');
-var uic = require('ls-uicontrol');
-var val = require('ls-validator');
-var bootstrap = require('bootstrap');
-
+var LoginView = require('./loginview.js').LoginView;
 var APIInterface = require('ls-api').APIInterface;
-var APIEndpoints = require('ls-api').APIEndpoints;
 var APIUI = require('ls-api-ui');
 
-const LOGIN_LANDING = "/control";
-const INPUT_USERNAME = $("#input-user");
-const INPUT_PASSWORD = $("#input-pass");
-const BTN_LOGIN = $("#btn-login");
-const CHECK_PERM = $("#checkbox-perm-session");
-
-var API = null;
-
-var pass_sel = null;
-var user_sel = null;
-
-var flag_login_ready = false;
-var defer_login_ready = () => { return !flag_login_ready; }
-
-const LOGIN_UI_DEFS = new uic.UIController({
-	'INPUT_USERNAME': new uic.UIInput({
-		elem: INPUT_USERNAME,
-		cond: () => true,
-		enabler: null,
-		attach: {
-			'keyup': (event) => {
-				if (
-					event.key == 'Enter'
-					&& LOGIN_UI_DEFS.get('INPUT_USERNAME').get().length
-					&& LOGIN_UI_DEFS.get('INPUT_PASSWORD').get().length
-				) {
-					login();
-				}
-			}
-		},
-		defer: defer_login_ready,
-		mod: null,
-		getter: elem => elem.val(),
-		setter: null,
-		clearer: null
-	}),
-	'INPUT_PASSWORD': new uic.UIInput({
-		elem: INPUT_PASSWORD,
-		cond: () => true,
-		enabler:null,
-		attach: {
-			'keyup': (event) => {
-				if (
-					event.key == 'Enter'
-					&& LOGIN_UI_DEFS.get('INPUT_USERNAME').get().length
-					&& LOGIN_UI_DEFS.get('INPUT_PASSWORD').get().length
-				) {
-					login();
-				}
-			}
-		},
-		defer: defer_login_ready,
-		mod: null,
-		getter: elem => elem.val(),
-		setter: null,
-		clearer: null
-	}),
-	'BTN_LOGIN': new uic.UIButton({
-		elem: BTN_LOGIN,
-		cond: () => true,
-		enabler: null,
-		attach: { 'click': async () => { await login(); } },
-		defer: defer_login_ready
-	})
-});
-
-function login_redirect(uri) {
-	window.location.assign(uri);
-}
-
-async function login() {
-	try {
-		await API.login(
-			INPUT_USERNAME.val(),
-			INPUT_PASSWORD.val(),
-			CHECK_PERM.is(":checked")
-		);
-	} catch (e) {
-		if (e instanceof APIError) {
-			if (e.response.error === APIError.codes.API_E_INCORRECT_CREDS) {
-				login_redirect("/login?failed=1");
-				return;
-			} else {
-				APIUI.handle_error(e);
-				return;
-			}
-		} else {
-			throw e;
-		}
-	}
-	if (CHECK_PERM.is(":checked")) {
-		login_redirect('/app');
-		return;
-	}
-	login_redirect(LOGIN_LANDING);
-}
-
 $(document).ready(async () => {
-	API = new APIInterface({standalone: false});
+	let API = new APIInterface({standalone: false});
 	try {
 		await API.init();
 	} catch (e) {
 		APIUI.handle_error(e);
 		return;
 	}
-	user_sel = new val.ValidatorSelector(
-		INPUT_USERNAME,
-		null,
-		[new val.StrValidator({
-			min: 1,
-			max: null,
-			regex: null
-		}, '')]
-	);
-	pass_sel = new val.ValidatorSelector(
-		INPUT_PASSWORD,
-		null,
-		[new val.StrValidator({
-			min: 1,
-			max: null,
-			regex: null
-		}, '')]
-	);
-	(val_trigger = new val.ValidatorTrigger(
-		[user_sel, pass_sel],
-		(valid) => {
-			LOGIN_UI_DEFS.get('BTN_LOGIN').enabled(valid);
-		}
-	)).trigger();
-	flag_login_ready = true;	
+
+	let view = new LoginView(API);
+	view.init();
 });
