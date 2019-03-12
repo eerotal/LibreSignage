280d3753d810e69892834103ce6d5da7a906d176
eerotal <eerotal@users.noreply.github.com>
2019-03-12 20:24:15 +0200

    build: Improve version check code formatting and error messages

--- build/scripts/npm_version.sh
+++ build/scripts/npm_version.sh
@@ -1,10 +1,16 @@
-#! /bin/sh
-## Compare NPM version to required (passed as argument)
+#!/bin/sh
+
+# Compare the current version of NPM to the required version
+# (passed as an argument)
+
+set -e
 CURRENT_NPM_VERSION=$(npm -v)
-version_check () { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1 )" \
-	!= "$1"; }
+version_check () {
+	test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1 )" != "$1"
+}
+
 if version_check $CURRENT_NPM_VERSION $1; then
-	echo "[ERROR] NPM version $1 required. Please update."
+	echo "[Error] NPM version $1 required. Please update."
 	exit 1
 else
 	exit 0
