cfbdc1c43acb2030b69546254af0bce6ebb596c5
eerotal <eerotal@users.noreply.github.com>
2019-04-25 18:45:05 +0300

    Revert "api: Implement orphan sessions"
    This reverts commit 99997a8dc958cdd0f158eee883bfb3eb8e8cb01c.

--- src/api/endpoint/auth/auth_session_renew.php
+++ src/api/endpoint/auth/auth_session_renew.php
@@ -3,18 +3,13 @@
 *  ====>
 *
 *  Request a session renewal. The previous authentication token
-*  is automatically expired when this endpoint is called. If this
-*  endpoint is called with permanent or orphan sessions, the session
-*  is not renewed and this endpoint returns 'renewed' => FALSE.
+*  is automatically expired when this endpoint is called.
 *
 *  **Request:** POST, application/json
 *
 *  Return value
-*    * renewed = Whether the session was renewed or not.
 *    * session = An associative array with the latest session data.
-*                This array also contains the session token under 'token'
-*                when the session is renewed.
-*    * error   = An error code or API_E_OK on success.
+*    * error = An error code or API_E_OK on success.
 *
 *  <====
 */
@@ -29,55 +24,52 @@ $AUTH_SESSION_RENEW = new APIEndpoint([
 	APIEndpoint::REQ_AUTH       => TRUE
 ]);
 
-$current_session = $AUTH_SESSION_RENEW->get_session();
-$current_user = $AUTH_SESSION_RENEW->get_caller();
+$session = $AUTH_SESSION_RENEW->get_session();
+$new_token = $session->renew();
+$AUTH_SESSION_RENEW->get_caller()->write();
 
-// Don't renew permanent or orphan sessions.
-if ($current_session->is_permanent() || $current_session->is_orphan()) {
-	$AUTH_SESSION_RENEW->resp_set([
-		'renewed' => FALSE,
-		'session' => $current_session->export(FALSE, FALSE),
-		'error' => API_E_OK
-	]);
-	$AUTH_SESSION_RENEW->send();
+/*
+*  Set the session cookies. Note that the server setting these
+*  cookies is merely a convenience feature for web browser clients
+*  that need to access the LibreSignage web interface too. Other
+*  clients can ignore these cookies if so desired.
+*/
+$exp = 0;
+if ($session->is_permanent()) {
+	$exp = PERMACOOKIE_EXPIRE;
+} else {
+	$exp = $session->get_created() + $session->get_max_age();
 }
-
-$new_session = $current_user->session_renew_by_id($current_session->get_id());
-$current_user->write();
-
-// Set the new session cookies.
-$exp = $new_session->get_created() + $new_session->get_max_age();
 setcookie(
 	$name = 'session_token',
-	$value = $new_session->get_token(),
+	$value = $new_token,
 	$expire = $exp,
 	$path = '/'
 );
 setcookie(
 	$name = 'session_created',
-	$value = $new_session->get_created(),
+	$value = $session->get_created(),
 	$expire = $exp,
 	$path = '/'
 );
 setcookie(
 	$name = 'session_max_age',
-	$value = $new_session->get_max_age(),
+	$value = $session->get_max_age(),
 	$expire = $exp,
 	$path = '/'
 );
 setcookie(
 	$name = 'session_permanent',
-	$value = $new_session->is_permanent() ? '1' : '0',
+	$value = $session->is_permanent() ? '1' : '0',
 	$expire = $exp,
 	$path = '/'
 );
 
-$AUTH_SESSION_RENEW->resp_set([
-	'renewed' => TRUE,
+$AUTH_SESSION_RENEW->resp_set(array(
 	'session' => array_merge(
-		$new_session->export(FALSE, FALSE),
-		['token' => $new_session->get_token()]
+		$AUTH_SESSION_RENEW->get_session()->export(FALSE, FALSE),
+		[ 'token' => $new_token ]
 	),
 	'error' => API_E_OK
-]);
+));
 $AUTH_SESSION_RENEW->send();
