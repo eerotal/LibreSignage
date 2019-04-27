928cee03c088134eb40b4fbd1b8a4c8fbf2eaa72
Derek Chafin <infomaniac50@gmail.com>
2019-04-26 20:42:01 -0500

    Remove redundant api/error.php require statement.

--- src/api/modules/module.php
+++ src/api/modules/module.php
@@ -4,8 +4,6 @@
 *  Base module class for API modules used in the API.
 */
 
-require_once(LIBRESIGNAGE_ROOT.'/api/error.php');
-
 abstract class APIModule {
 	public function __construct() {}
 	abstract function run(APIEndpoint $endpoint);
