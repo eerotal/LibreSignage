13dadf25ffcd338775eaedbbb3f88b7e1b1203ec
Derek Chafin <infomaniac50@gmail.com>
2019-04-26 20:45:28 -0500

    Remove include_guard.php

--- src/control/editor/popups/quick_help.php
+++ src/control/editor/popups/quick_help.php
@@ -1,7 +1,4 @@
 <!-- Quick help popup for the LibreSignage editor. -->
-<?php
-	require_once(LIBRESIGNAGE_ROOT.'/common/php/include_guard.php');
-?>
 <div id="quick-help" class="popup">
 	<?php require_once(LIBRESIGNAGE_ROOT.DOC_HTML_DIR.'/keyboard_shortcut_cheatsheet.html') ?>
 	<?php require_once(LIBRESIGNAGE_ROOT.DOC_HTML_DIR.'/markup.html') ?>
