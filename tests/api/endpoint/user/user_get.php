f9ab32882877aa5485092b878ffa844b09f6947d
Eero Talus <eerotal@mbnet.fi>
2019-07-08 22:36:17 +0300

    tests: Remove commented-out code

--- tests/api/endpoint/user/user_get.php
+++ tests/api/endpoint/user/user_get.php
@@ -72,51 +72,6 @@ class user_get extends APITestCase {
 		];
 	}
 
-	/*public function test_invalid_request_error_with_missing_user_parameter(): void {
-		$this->api->login('admin', 'admin');
-
-		$resp = $this->api->call(
-			$this->get_endpoint_method(),
-			$this->get_endpoint_uri(),
-			[],
-			[],
-			TRUE
-		);
-		$this->assert_api_failed($resp, 'API_E_INVALID_REQUEST');
-
-		$this->api->logout();
-	}
-
-	public function test_invalid_request_error_with_nonexistent_user(): void {
-		$this->api->login('admin', 'admin');
-
-		$resp = $this->api->call(
-			$this->get_endpoint_method(),
-			$this->get_endpoint_uri(),
-			['user' => 'nouser'],
-			[],
-			TRUE
-		);		
-		$this->assert_api_failed($resp, 'API_E_INVALID_REQUEST');
-
-		$this->api->logout();
-	}
-
-	public function test_invalid_request_error_with_empty_user_parameter(): void {
-		$this->api->login('admin', 'admin');
-
-		$resp = $this->api->call(
-			$this->get_endpoint_method(),
-			$this->get_endpoint_uri(),
-			['user' => ''],
-			[],
-			TRUE
-		);		
-		$this->assert_api_failed($resp, 'API_E_INVALID_REQUEST');
-
-		$this->api->logout();
-	}*/
-
 	public function test_endpoint_not_authorized_for_non_admin_users(): void {
 		$this->api->login('user', 'user');
 
