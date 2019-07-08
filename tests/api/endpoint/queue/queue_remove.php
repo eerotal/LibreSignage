80377798ea48bcc10206b9320ff740deb5b7f802
Eero Talus <eerotal@mbnet.fi>
2019-07-08 22:52:22 +0300

    tests: Add tests for queue_remove.php

--- tests/api/endpoint/queue/queue_remove.php
+++ tests/api/endpoint/queue/queue_remove.php
@@ -3,7 +3,7 @@
 use \classes\APITestCase;
 use \api\HTTPStatus;
 
-class queue_create extends APITestCase {
+class queue_remove extends APITestCase {
 	use traits\TestEndpointNotAuthorizedWithoutLogin;
 
 	const TEST_QUEUE_NAME = 'test_queue';
@@ -12,14 +12,30 @@ class queue_create extends APITestCase {
 		parent::setUp();
 
 		$this->set_endpoint_method('POST');
-		$this->set_endpoint_uri('queue/queue_create.php');
+		$this->set_endpoint_uri('queue/queue_remove.php');
+
+		// Create an initial slide to remove.
+		$this->api->login('admin', 'admin');
+		$this->api->call(
+			'POST',
+			'queue/queue_create.php',
+			['name' => self::TEST_QUEUE_NAME],
+			[],
+			TRUE
+		);
+		$this->api->logout();
 	}
 
 	/**
 	* @dataProvider params_provider
 	*/
-	public function test_fuzz_params(array $params, int $error): void {
-		$this->api->login('admin', 'admin');
+	public function test_fuzz_params(
+		string $user,
+		string $pass,
+		array $params,
+		int $error
+	): void {
+		$this->api->login($user, $pass);
 
 		$resp = $this->api->call_return_raw_response(
 			$this->get_endpoint_method(),
@@ -39,28 +55,47 @@ class queue_create extends APITestCase {
 	public static function params_provider(): array {
 		return [
 			'Valid parameters' => [
+				'admin',
+				'admin',
 				['name' => self::TEST_QUEUE_NAME],
 				HTTPStatus::OK
 			],
 			'Missing name parameter' => [
+				'admin',
+				'admin',
 				[],
 				HTTPStatus::BAD_REQUEST
 			],
 			'Empty name parameter' => [
+				'admin',
+				'admin',
 				['name' => ''],
 				HTTPStatus::BAD_REQUEST
 			],
 			'Wrong type for name paremeter' => [
+				'admin',
+				'admin',
 				['name' => TRUE],
 				HTTPStatus::BAD_REQUEST
+			],
+			'User display tries to remove slide of user admin' => [
+				'display',
+				'display',
+				['name' => self::TEST_QUEUE_NAME],
+				HTTPStatus::UNAUTHORIZED
+			],
+			'User user tries to remove slide of user admin' => [
+				'user',
+				'user',
+				['name' => self::TEST_QUEUE_NAME],
+				HTTPStatus::UNAUTHORIZED
 			]
 		];
 	}
 
 	public function tearDown(): void {
-		$this->api->logout();
+		// Make sure the initial slide is removed.
 		$this->api->login('admin', 'admin');
-
 		$this->api->call(
 			'POST',
 			'queue/queue_remove.php',
@@ -68,7 +103,6 @@ class queue_create extends APITestCase {
 			[],
 			TRUE
 		);
-
 		$this->api->logout();
 	}
 }
