9c55bedfb84ca523a888748411fef8c9070cc8f2
Eero Talus <eerotal@mbnet.fi>
2019-07-10 12:19:27 +0300

    tests: Implement tests for auth_get_sessions.php

---
+++ tests/api/endpoint/auth/auth_get_sessions.php
@@ -0,0 +1,24 @@
+<?php
+
+use \classes\APITestCase;
+
+class auth_get_sessions extends APITestCase {
+	use \traits\TestEndpointNotAuthorizedWithoutLogin;
+
+	public function setUp(): void {
+		parent::setUp();
+
+		$this->set_endpoint_method('GET');
+		$this->set_endpoint_uri('auth/auth_get_sessions.php');
+	}
+
+	public function test_is_response_schema_correct(): void {
+		$this->call_api_and_check_response_schema(
+			[],
+			[],
+			dirname(__FILE__).'/schemas/auth_get_sessions.schema.json',
+			'admin',
+			'admin'
+		);
+	}
+}
