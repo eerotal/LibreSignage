cf41e4da2ec40a574c3ae5dd54e160f923a8ede2
Eero Talus <eerotal@mbnet.fi>
2019-07-11 10:00:41 +0300

    tests: Write tests for auth_session_renew.php

---
+++ tests/api/endpoint/auth/auth_session_renew.php
@@ -0,0 +1,24 @@
+<?php
+
+use \classes\APITestCase;
+
+class auth_session_renew extends APITestCase {
+	use \traits\TestEndpointNotAuthorizedWithoutLogin;
+
+	public function setUp(): void {
+		parent::setUp();
+
+		$this->set_endpoint_method('POST');
+		$this->set_endpoint_uri('auth/auth_session_renew.php');
+	}
+
+	public function test_is_response_schema_correct() {
+		$this->call_api_and_check_response_schema(
+			[],
+			[],
+			dirname(__FILE__).'/schemas/auth_session_renew.schema.json',
+			'admin',
+			'admin'
+		);
+	}	
+}
