7b36c5de98e555484a5d2a84aebb80138e394d0c
Eero Talus <eerotal@mbnet.fi>
2019-07-12 11:57:22 +0300

    tests: Write tests for slide_list.php

---
+++ tests/api/endpoint/slide/slide_list.php
@@ -0,0 +1,24 @@
+<?php
+
+use \classes\APITestCase;
+
+class slide_list extends APITestCase {
+	use \traits\TestEndpointNotAuthorizedWithoutLogin;
+
+	public function setUp(): void {
+		parent::setUp();
+
+		$this->set_endpoint_method('GET');
+		$this->set_endpoint_uri('slide/slide_list.php');
+	}
+
+	public function test_is_response_schema_correct() {
+		$this->call_api_and_check_response_schema(
+			[],
+			[],
+			dirname(__FILE__).'/schemas/slide_list.schema.json',
+			'admin',
+			'admin'
+		);
+	}
+}
