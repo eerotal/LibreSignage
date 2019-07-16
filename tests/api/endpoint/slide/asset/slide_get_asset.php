fda2a7a8290aa24c58d997b73eeff074b25b2ec5
Eero Talus <eerotal@mbnet.fi>
2019-07-16 16:35:14 +0300

    tests: Write more tests for slide_get_asset.php

--- tests/api/endpoint/slide/asset/slide_get_asset.php
+++ tests/api/endpoint/slide/asset/slide_get_asset.php
@@ -52,6 +52,108 @@ class slide_get_asset extends APITestCase {
 		}
 	}
 
+	/**
+	* @dataProvider params_provider
+	*/
+	public function test_fuzz_params(
+		string $user,
+		string $pass,
+		array $params,
+		int $error
+	) {
+		$this->call_api_and_assert_failed(
+			$params,
+			[],
+			$error,
+			$user,
+			$pass
+		);
+	}
+
+	public static function params_provider(): array {
+		return [
+			'Allowed for group admin' => [
+				'admin',
+				'admin',
+				[
+					'id' => self::TEST_SLIDE_ID,
+					'name' => basename(self::TEST_ASSET_PATH)
+				],
+				HTTPStatus::OK
+			],
+			'Allowed for group user' => [
+				'user',
+				'user',
+				[
+					'id' => self::TEST_SLIDE_ID,
+					'name' => basename(self::TEST_ASSET_PATH)
+				],
+				HTTPStatus::OK
+			],
+			'Allowed for group display' => [
+				'display',
+				'display',
+				[
+					'id' => self::TEST_SLIDE_ID,
+					'name' => basename(self::TEST_ASSET_PATH)
+				],
+				HTTPStatus::OK
+			],
+			'Missing id parameter' => [
+				'admin',
+				'admin',
+				[
+					'name' => basename(self::TEST_ASSET_PATH)
+				],
+				HTTPStatus::BAD_REQUEST
+			],
+			'Empty id parameter' => [
+				'admin',
+				'admin',
+				[
+					'id' => '',
+					'name' => basename(self::TEST_ASSET_PATH)
+				],
+				HTTPStatus::BAD_REQUEST
+			],
+			'Nonexistent slide id' => [
+				'admin',
+				'admin',
+				[
+					'id' => '11bbcceeddff',
+					'name' => basename(self::TEST_ASSET_PATH)
+				],
+				HTTPStatus::BAD_REQUEST
+			],
+			'Missing name parameter' => [
+				'admin',
+				'admin',
+				[
+					'id' => self::TEST_SLIDE_ID
+				],
+				HTTPStatus::BAD_REQUEST
+			],
+			'Empty name parameter' => [
+				'admin',
+				'admin',
+				[
+					'id' => self::TEST_SLIDE_ID,
+					'name' => ''
+				],
+				HTTPStatus::NOT_FOUND
+			],
+			'Nonexistent asset' => [
+				'admin',
+				'admin',
+				[
+					'id' => self::TEST_SLIDE_ID,
+					'name' => 'nosuchfile.png'
+				],
+				HTTPStatus::NOT_FOUND
+			]
+		];
+	}
+
 	public function test_received_asset_matches_original() {
 		$this->api->login('admin', 'admin');
 		$resp = $this->api->call_return_raw_response(
