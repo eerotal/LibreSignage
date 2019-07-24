874da978682447f5bdd79d1a1fe36706c9ec5ea1
Eero Talus <eerotal@mbnet.fi>
2019-07-24 17:44:17 +0300

    tests: Remove unused code

--- tests/common/classes/TestConfig.php
+++ tests/common/classes/TestConfig.php
@@ -4,9 +4,6 @@ namespace libresignage\tests\common\classes;
 
 final class TestConfig {
 	const LS_DIST_PREFIX = 'dist/';
-	const INCLUDE_PATHS = [
-		__DIR__.'/..'
-	];
 
 	public function __construct() {
 		echo "[Info] Configuring the unit test framework...\n";
@@ -20,16 +17,6 @@ final class TestConfig {
 			if (!(\error_reporting() & $severity)) { return false; }
 			throw new \ErrorException($msg, 0, $severity, $file, $line);
 		});
-	}
-
-	public static function autoload_symbol(string $name) {
-		$tmp = str_replace('\\', '/', $name);
-		foreach (TestConfig::INCLUDE_PATHS as $i) {
-			try {
-				include(implode('/', [$i, $tmp.'.php']));
-			} catch (\Exception $e) { continue; }
-			break;
-		}
 	}
 
 	public static function setup_symbol_autoloading() {
@@ -65,7 +52,5 @@ final class TestConfig {
 			}
 			$autoloader->setPsr4($namespace, $paths);
 		}
-
-		//\spl_autoload_register(['classes\TestConfig', 'autoload_symbol']);
 	}
 }
